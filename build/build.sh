4b6c4aa0a2493f15a5162fb6a7e88e6d5eac331f
minskiter <minskiter@qq.com>
2021-01-21 18:34:27 +0800

    [update] 优化二进制体积

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 rm -rf release
 mkdir release
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o ./release/fzuconnect_linux_arm
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./release/fzuconnect_win_amd64.exe
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./release/fzuconnect_linux_amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o ./release/fzuconnect_linux_arm -ldflags="-w -s"
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./release/fzuconnect_win_amd64.exe -ldflags="-w -s"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./release/fzuconnect_linux_amd64 -ldflags="-w -s"
