5b6312c37ce191152d061586c0623252a91c44d1
minskiter <minskiter@qq.com>
2024-10-14 21:27:27 +0800

    [update] update loop

--- backservice/backservice.go
+++ backservice/backservice.go
@@ -47,9 +47,9 @@ func (p *Program) NetStatus() error {
 		p.Logger.Error(err)
 	}
 	if res.UserId != "" {
-		p.Logger.Info(fmt.Sprintf("当前用户 %s 已登陆", res.UserId))
+		p.Logger.Info(fmt.Sprintf("current user %s already login", res.UserId))
 	} else {
-		p.Logger.Info((fmt.Sprintf("当前校园网未登陆")))
+		p.Logger.Info("logout.")
 	}
 	return nil
 }
@@ -63,37 +63,37 @@ func (p *Program) RunOnce(session *fzuconnect.LoginSession) {
 	if res.UserId != session.Username {
 		// 强制下线当前用户
 		if res.UserId != "" {
-			p.Logger.Info(fmt.Sprintf("强制下线当前用户 %s", res.UserId))
+			p.Logger.Info(fmt.Sprintf("logout %s", res.UserId))
 			res, err := session.Logout()
 			if err != nil {
 				p.Logger.Error(err)
 			}
-			p.Logger.Info(fmt.Sprintf("强制下线结果: %s", res.Result))
+			p.Logger.Info(fmt.Sprintf("logout result: %s", res.Result))
 		}
 		// 登陆目标用户
 		{
-			p.Logger.Info(fmt.Sprintf("登陆用户 %s", session.Username))
+			p.Logger.Info(fmt.Sprintf("%s login", session.Username))
 			res, err := session.Connect()
 			if err != nil {
 				p.Logger.Error(err)
 			}
-			p.Logger.Info(fmt.Sprintf("登陆结果: %s", res.Result))
+			p.Logger.Info(fmt.Sprintf("result: %s", res.Result))
 		}
 	} else {
-		p.Logger.Info(fmt.Sprintf("当前用户 %s 已登陆", res.UserId))
+		p.Logger.Info(fmt.Sprintf("current user %s already login", res.UserId))
 	}
 }
 
 // run 程序运行
 func (p *Program) Run(session *fzuconnect.LoginSession) {
 	p.RunOnce(session)
-	for range time.Tick(time.Minute * 5) { // 每隔5分钟登陆校园网
+	for range time.Tick(time.Minute) { // 每隔1分钟登陆校园网
 		p.RunOnce(session)
 	}
 }
 
 // Stop 程序结束
 func (p *Program) Stop(s service.Service) error {
-	p.Logger.Info("结束服务")
+	p.Logger.Info("stop service")
 	return nil
 }
